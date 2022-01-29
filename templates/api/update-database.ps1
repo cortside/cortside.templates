49458ccfc5b2be83a856c95e9cad89c4966859be
Cort Schaefer <cort@eschaefer.net>
2022-01-29 00:03:06 -0700

    cleanup

--- templates/api/update-database.ps1
+++ templates/api/update-database.ps1
@@ -1,18 +1,19 @@
-[CmdletBinding()]
 Param
 (
-        [Parameter(Mandatory=$false)][string]$server = "localhost",
+	[Parameter(Mandatory = $false)][string]$server = "localhost",
         [Parameter(Mandatory=$false)][string]$database = "WebApiStarter",
-		[Parameter(Mandatory=$false)][string]$username = "",
-		[Parameter(Mandatory=$false)][string]$password = "",
-		[Parameter(Mandatory=$false)][string]$ConnectionString = "",
-		[Parameter(Mandatory=$false)][switch]$UseIntegratedSecurity
+	[Parameter(Mandatory = $false)][string]$username = "",
+	[Parameter(Mandatory = $false)][string]$password = "",
+	[Parameter(Mandatory = $false)][string]$ConnectionString = "",
+	[Parameter(Mandatory = $false)][switch]$UseIntegratedSecurity,
+	[Parameter(Mandatory = $false)][switch]$CreateDatabase
 )
 
 $ErrorActionPreference = 'Stop'; $ProgressPreference = 'SilentlyContinue';
 
+
 try {
-    # ErrorAction must be Stop in order to trigger catch
+	# ErrorAction must be Stop in order to trigger catch
 	Import-Module SqlServer -ErrorAction Stop
 }
 catch {
@@ -32,32 +33,50 @@ if ($ConnectionString) {
 	if (!($UseIntegratedSecurity.IsPresent)) {
 		$conn.TryGetValue('User ID', [ref]$username)
 		$conn.TryGetValue('Password', [ref]$password)
-	} else {
+	}
+ else {
 		Write-Output "using integrated security"
+	}
+}
+
+if ($CreateDatabase.IsPresent) {
+	Write-Output "Creating database..."
+	if ($username -eq "") {
+		invoke-sqlcmd -Server "$server" -Query "CREATE database $database"
+	}
+ else {
+		$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+		$creds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+		invoke-sqlcmd -Credential $creds -Server $server -Query "CREATE database $database"
 	}
 }
 
 echo "update database..."
 
-$scripts = @()
-gci -Recurse @("src/sql/*.migration.sql") | % {
-	$scripts += $_.FullName
-}
-gci -Recurse @("src/sql/*.table.sql") | % {
-	$scripts += $_.FullName
-}
-gci -Recurse @("src/sql/*.proc.sql") | % {
-	$scripts += $_.FullName
-}
-gci -Recurse @("src/sql/*.trigger.sql") | % {
-	$scripts += $_.FullName
-}
-gci -Recurse @("src/sql/*.data.sql") | % {
-	$scripts += $_.FullName
-}
 
-# TODO: figure out how to sort the scripts so that the .migration file is first OR truncate the file extensions and look for each in the foreach loop
-# $scripts = $scripts | sort
+$scripts = @()
+gci -Recurse @(".\src\sql\migration\*.sql") | % {
+	$scripts += $_.FullName
+}
+gci -Recurse @(".\src\sql\table\*.sql") | % {
+	$scripts += $_.FullName
+}
+gci -Recurse @(".\src\sql\data\*.sql") | % {
+	$scripts += $_.FullName
+}
+gci -Recurse @(".\src\sql\proc\*.sql") | % {
+	$scripts += $_.FullName
+}
+gci -Recurse @(".\src\sql\trigger\*.sql") | % {
+	$scripts += $_.FullName
+}
+gci -Recurse @(".\src\sql\view\*.sql") | % {
+	$scripts += $_.FullName
+}
+#gci -Recurse @(".\src\sql\release\*.sql") | % {
+#	$scripts += $_.FullName
+#}
+
 
 echo "Server: $Server"
 echo "Database: $database"
@@ -68,10 +87,11 @@ foreach ($script in $scripts) {
 	$log = $script -replace "(.*).sql(.*)", '$1.log$2'
 	
 	if ($username -eq "") {
-		Invoke-SqlCmd  -Server $server -Database $database -inputFile "$script" | Out-File -FilePath "$log"
-	} else  {
+		invoke-sqlcmd -Server "$server" -Database $database -inputFile "$script" | Out-File -FilePath "$log"
+	}
+ else {
 		$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
-		$creds = New-Object System.Management.Automation.PSCredential ($username,$secpasswd)
+		$creds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
 		
 		invoke-sqlcmd -Credential $creds -Server $server -Database $database -inputFile "$script" | Out-File -FilePath "$log"
 	}
