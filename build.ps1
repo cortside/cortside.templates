9db45948e85b99df3bf24078fd9f75f78a24c328
Cort Schaefer <cort@eschaefer.net>
2022-03-11 21:10:08 -0700

    added notes; updated symbols for replacement

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ rm -Force -Recurse temp
 dotnet new --uninstall cortside.templates
 dotnet new --install .\artifacts\*.nupkg
 
-dotnet new cortside-api --output ./temp --name Foo.Bar --organization Foo --service Bar
+dotnet new cortside-api --output ./temp --name Foo.Bar --company Foo --product Bar
 
 dotnet build ./temp/src
 dotnet test ./temp/src
