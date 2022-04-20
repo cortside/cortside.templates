cfad88db9f1e2be1f7ed9cc68d96e12603f481d7
Cort Schaefer <cort@eschaefer.net>
2022-04-19 19:53:44 -0600

    rename template, add new editorconfig template, add helper script to help update newer common changes that are not nuget packages

---
+++ templates/api-powershell/update-template.ps1
@@ -0,0 +1,11 @@
+# make sure latest version of cortside.templates is installed
+dotnet new --install cortside.templates
+
+# update powershell scripts from root
+dotnet new cortside-api-powershell --force
+
+# update .editorconfig
+dotnet new cortside-api-editorconfig --force
+
+# update dockerfile and supporting shell and powershell scripts
+#dotnet new cortside-api-deployment --force
