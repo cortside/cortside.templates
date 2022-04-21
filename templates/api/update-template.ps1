ded107f8f2b09942d6ccd3bcdbd749e40e2bf1dd
Cort Schaefer <cort@eschaefer.net>
2022-04-20 19:22:51 -0600

    updates to powershell

--- templates/api/update-template.ps1
+++ templates/api/update-template.ps1
@@ -2,12 +2,12 @@
 dotnet new --install cortside.templates
 
 # update powershell scripts from root
-dotnet new cortside-api-powershell --force --name Acme.ShoppingCart --company Acme --product ShoppingCart
+dotnet new cortside-api-powershell --force --output ./ --name Acme.ShoppingCart --company Acme --product ShoppingCart
 
 # update .editorconfig
-dotnet new cortside-api-editorconfig --force --name Acme.ShoppingCart --company Acme --product ShoppingCart
+dotnet new cortside-api-editorconfig --force --output ./ --name Acme.ShoppingCart --company Acme --product ShoppingCart
 
 # update dockerfile and supporting shell and powershell scripts
 if (Test-Path -path "deploy") {
-	dotnet new cortside-api-deploy --force --name Acme.ShoppingCart --company Acme --product ShoppingCart
+	dotnet new cortside-api-deploy --force --output ./ --name Acme.ShoppingCart --company Acme --product ShoppingCart
 }
