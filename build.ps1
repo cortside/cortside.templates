3907ada06b516a034d2e1b3f8a71c7397364407b
Cort Schaefer <cort@eschaefer.net>
2023-10-18 19:18:22 -0600

    [identityserver4] updated chiron example in template form

--- build.ps1
+++ build.ps1
@@ -12,10 +12,14 @@ dotnet new uninstall cortside.templates
 dotnet new install .\artifacts\*.nupkg
 
 dotnet new cortside-api --output ./temp/api --name Foo.Bar --company Foo --product Bar
-dotnet new cortside-web --output ./temp/web --name Foo.Bar --company Foo --product Bar
-
 dotnet build ./temp/api/src
 dotnet test ./temp/api/src
+
+dotnet new cortside-ids4 --output ./temp/ids4 --name Foo.IdentityServer --company Foo
+dotnet build ./temp/ids4/src
+dotnet test ./temp/ids4/src
+
+dotnet new cortside-web --output ./temp/web --name Foo.Bar --company Foo --product Bar
 
 if (Test-Path temp) {
 	rm -Force -Recurse temp
